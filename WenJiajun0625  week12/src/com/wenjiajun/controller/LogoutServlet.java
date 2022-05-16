64f4daab2192b57ab0dfb20efc7e6fe3f82aeb4a
wenwen <2639167576@qq.com>
2022-05-16 11:27:35 +0800

    week12

---
+++ WenJiajun0625  week12/src/com/wenjiajun/controller/LogoutServlet.java
@@ -0,0 +1,24 @@
+package com.wenjiajun.controller;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "LogoutServlet", value = "/logout")
+public class LogoutServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.getSession(false).invalidate();
+        request.setAttribute("message","You have successfully lugget out!");
+        request.getRequestDispatcher("WEB-INF/views/login.jsp").forward(request,response);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
